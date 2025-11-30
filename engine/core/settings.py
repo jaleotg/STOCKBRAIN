e34dd035740778fa3e30a32ce0ad8f74e3768e15
Leo <leo@kmoidcpmis.cloud>
2025-11-30 11:48:55 +0000

    ustawienie adresow serwera IP - dodalem 192.168.0.200 oraz 100.120.14.89

--- engine/core/settings.py
+++ engine/core/settings.py
@@ -10,7 +10,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "dev-secret-key-change-in-production"
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "192.168.0.200", #lokal Hydrotec LAN
+    "localhost",
+    "127.0.0.1",
+    "100.120.14.89" #tailscale
+]
 
 
 # ------------------------------------------
